b53b5e5c3a1928e155b2d703d18a6dfbf51a0041
Anka <runner@iad20-eo1211-b25d5d21-ee71-4eed-94c9-62766883525e-CA2445877F07.local>
2026-08-03 15:37:06 +0000

    yaml-update-last-modified-date 1.1.2

--- Formula/yaml-update-last-modified-date.rb
+++ Formula/yaml-update-last-modified-date.rb
@@ -1,8 +1,8 @@
 class YamlUpdateLastModifiedDate < Formula
   desc "Set the last modified date in YAML front matter of edited markdown files"
   homepage "https://github.com/proinsias/yaml-update-last-modified-date"
-  url "https://github.com/proinsias/yaml-update-last-modified-date/archive/refs/tags/v1.1.1.tar.gz"
-  sha256 "69c69c0b5ab4ca08230a6617473b9fed1af986bf7e5ba1a998b37135f6fe05af"
+  url "https://github.com/proinsias/yaml-update-last-modified-date/archive/refs/tags/v1.1.2.tar.gz"
+  sha256 "a09ac7cbaac4265330e8a08aba8824f5b59cd064f1101c14335194e4c616580d"
 
   def install
     bin.install "yaml-update-last-modified-date"
